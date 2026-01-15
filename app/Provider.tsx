41719c0d0b0a685c9c253a05aca28a3182b4038a
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2026-01-15 14:47:05 +0530

    neon db setup drizlle and user setup

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { useClerk } from '@clerk/nextjs'
+import axios from 'axios'
+import { UserDetailContext } from '@/context/UserDetailContext'
+import { set } from 'date-fns'
 
 function Provider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+
+  const { user } = useClerk();
+  const [userDetail, setUserDetail] = useState(undefined);
+
+  React.useEffect(() => {
+    user && CreateNewUser();
+  }, [user])
+
+  const CreateNewUser = async () => {
+    const result = await axios.post('/api/user', {});
+    setUserDetail(result?.data);
+  }
   return (
+
     <NextThemesProvider {...props}>
-      {children}
-    </NextThemesProvider>
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        {children}
+      </UserDetailContext.Provider>
+    </NextThemesProvider >
+
   )
 }
 
---
+++ context/UserDetailContext.tsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+
+export const UserDetailContext = createContext({
+    userDetail: undefined,
+    setUserDetail: (detail: any) => { }
+})
